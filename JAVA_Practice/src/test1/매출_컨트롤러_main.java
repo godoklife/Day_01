51529d7115872cea63b5dd9890e27517ab5414b4
godoklife <96913067+godoklife@users.noreply.github.com>
2022-03-20 16:21:13 +0900

    111

--- JAVA_Practice/src/test1/매출_컨트롤러_main.java
+++ JAVA_Practice/src/test1/매출_컨트롤러_main.java
@@ -32,20 +32,6 @@ public class 매출_컨트롤러_main {
 				System.out.println("월 입력\n입력예시)03월 : 3");
 				int month = scanner.nextInt();
 				ArrayList<매출> 검색결과출력용리스트 =  new ArrayList<>(메인컨트롤러.매출출력(year, month));
-//				
-//				
-//				for(int i=0; i<검색결과출력용리스트.size(); i++) {
-//					String tmp="";	// null 넣으면 널포인터에러때문에 그냥 공백 저장.
-//					int 임시매출누적=0;
-//					tmp = 검색결과출력용리스트.get(i).get날짜();
-//					for(int j=0; j<검색결과출력용리스트.size(); j++) {
-//						if(tmp.equals(검색결과출력용리스트.get(j).get날짜())) {
-//							임시매출누적 += 검색결과출력용리스트.get(j).get매출금액();
-//							검색결과출력용리스트.get(j).set매출금액(0);
-//						}
-//					}
-//					검색결과출력용리스트.get(i).set매출금액(임시매출누적);
-//				}
 				for(매출 tmp : 검색결과출력용리스트) {
 					System.out.printf("%d년 %2d월 %s일 매출 : %s원\n", year, month, tmp.get날짜(), tmp.get매출금액());
 				}
