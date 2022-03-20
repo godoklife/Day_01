f2f5d09634d9d36f448f175ec0c25cdc21ffce9e
godoklife <96913067+godoklife@users.noreply.github.com>
2022-03-20 14:30:14 +0900

    11

--- JAVA_Practice/src/test1/매출_컨트롤러_main.java
+++ JAVA_Practice/src/test1/매출_컨트롤러_main.java
@@ -7,6 +7,10 @@ public class 매출_컨트롤러_main {
 	public static void main(String[] args) {
 		컨트롤러 메인컨트롤러 = new 컨트롤러();
 		
+		
+		
+		
+		
 		Scanner scanner = new Scanner(System.in);
 		메인컨트롤러.매출파일로딩();
 			
@@ -23,12 +27,31 @@ public class 매출_컨트롤러_main {
 				System.out.println("월 입력\n입력예시)03월 : 3");
 				int month = scanner.nextInt();
 				ArrayList<매출> 검색결과출력용리스트 =  new ArrayList<>(메인컨트롤러.매출출력(year, month));
+				
+				
 				for(int i=0; i<검색결과출력용리스트.size(); i++) {
-					System.out.printf("%d년 %2d월 %s일 매출 : %s원\n", year, month, 검색결과출력용리스트.get(i).get날짜(), 검색결과출력용리스트.get(i).get매출금액());
-//					System.out.println(검색결과출력용리스트.get(i).get매출금액()+"원");
+					String tmp="";	// null 넣으면 널포인터에러때문에 그냥 공백 저장.
+					int 임시매출누적=0;
+					tmp = 검색결과출력용리스트.get(i).get날짜();
+					for(int j=0; j<검색결과출력용리스트.size(); j++) {
+						if(tmp.equals(검색결과출력용리스트.get(j).get날짜())) {
+							임시매출누적 += 검색결과출력용리스트.get(j).get매출금액();
+							검색결과출력용리스트.get(j).set매출금액(0);
+						}
+					}
+					검색결과출력용리스트.get(i).set매출금액(임시매출누적);
 				}
-			}
-			
+				
+				
+				for(매출 tmp : 검색결과출력용리스트) {
+					if(tmp.get매출금액()!=0){
+						System.out.printf("%d년 %2d월 %s일 매출 : %s원\n", year, month, tmp.get날짜(), tmp.get매출금액());
+					}else {
+						System.out.println("입력하신 날짜에는 매출이 없습니다.");
+					}
+				}
+				
+			}	
 		}
 	}
 }
